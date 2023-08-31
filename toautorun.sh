7d92504cebdb38579eb38b55a7dc9c0e127a18f9
Draco156 <uttamkn15@gmail.com>
2023-08-31 22:48:26 +0530

    Add the autorun file

---
+++ toautorun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#ADD THIS FILE IN YOUR AUTORUN SCRIPT (check README.MD)
+
+xwallpaper --zoom ~/Pictures/$(~/.config/Wally/save_wallpap.sh)
+wal -i ~/Pictures/$(~/.config/Wally/save_wallpap.sh)
+
+clear
+
+neofetch
