353d3dadece5f60b0ad5874dc2c35b59a04341df
Draco156 <uttamkn15@gmail.com>
2023-08-31 22:50:06 +0530

    Add the install script

---
+++ install_wally.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+mkdir ~/.config/Wally
+
+cp ./wallpaper-change.sh ~/.config/Wally
+cp ./save_wallpap.sh ~/.config/Wally
+cp ./toautorun.sh ~/.config/Wally
+cp ./README.md ~/.config/Wally
